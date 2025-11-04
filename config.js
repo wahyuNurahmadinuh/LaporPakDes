3eb8dff93e9ca35cf4676c50661d7d3897ed0efd
Wahyu Nurahmadinuh <152614992+wahyuNurahmadinuh@users.noreply.github.com>
2025-11-04 16:43:27 +0700

    Delete config.js - ubah total kode ke database offline(sql)

--- config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAZmkKMbsS2eeQhlABO_pch9HrkGVujGnk",
-  authDomain: "laporpakdes.firebaseapp.com",
-  projectId: "laporpakdes",
-  storageBucket: "laporpakdes.firebasestorage.app",
-  messagingSenderId: "558950220803",
-  appId: "1:558950220803:web:dc2ff1914d7b6b6da03a36",
-  measurementId: "G-ZMWMKTWQV4"
-}
